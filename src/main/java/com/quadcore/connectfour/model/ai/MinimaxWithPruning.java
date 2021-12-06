b37d8e8308f3e47023d7c906e24604d306e2585e
Ahmad Waleed <a.waleedothman@gmail.com>
2021-12-06 19:19:53 +0200

    MinimaxWithPruning

--- src/main/java/com/quadcore/connectfour/model/ai/MinimaxWithPruning.java
+++ src/main/java/com/quadcore/connectfour/model/ai/MinimaxWithPruning.java
@@ -5,25 +5,80 @@ import com.quadcore.connectfour.model.state.State;
 
 public class MinimaxWithPruning implements ConnectFourAI {
 
-    private final int depth;
+    private final int maxDepth;
+    private TreeNode root = null;
+    private int numberOfNodesExpanded = 0;
 
-    public MinimaxWithPruning(int depth) {
-        this.depth = depth;
+
+    public MinimaxWithPruning(int maxDepth) {
+        if (maxDepth < 1) throw new IllegalArgumentException("Minimax depth can't be less than one");
+        this.maxDepth = maxDepth;
     }
 
     @Override
     public TreeNode play(State state) {
-        return null;
+        if (state.isTerminal()) throw new IllegalArgumentException("Can't play on a terminal state");
+        numberOfNodesExpanded = 1;
+        root = new TreeNode(state, null);
+        TreeNode optimalMove = null;
+
+        double maxScore = Double.MIN_VALUE;
+        double alpha = Double.MIN_VALUE;
+        double beta = Double.MAX_VALUE;
+
+        for (State s : state.getNeighbours()) {
+            TreeNode stateTreeNode = new TreeNode(s, root);
+            double childVal = minimize(maxDepth - 1, stateTreeNode, alpha, beta);
+            if (childVal > maxScore) {
+                maxScore = childVal;
+                alpha = childVal;
+                optimalMove = stateTreeNode;
+            }
+        }
+
+        return optimalMove;
+    }
+
+    private double maximize(int depth, TreeNode node, double alpha, double beta) {
+        numberOfNodesExpanded++;
+        if (depth == 0 || node.getState().isTerminal())
+            return node.getState().evaluate();
+
+        double max = Double.MIN_VALUE;
+        for (State s : node.getState().getNeighbours()) {
+            TreeNode neighbourTreeNode = new TreeNode(s, node);
+            max = Math.max(max, this.minimize(depth - 1, neighbourTreeNode, alpha, beta));
+            if (max > alpha) alpha = max;
+            if (alpha >= beta) break;
+        }
+
+        return max;
+    }
+
+    private double minimize(int depth, TreeNode node, double alpha, double beta) {
+        numberOfNodesExpanded++;
+        if (depth == 0 || node.getState().isTerminal())
+            return node.getState().evaluate();
+
+        double min = Double.MAX_VALUE;
+        for (State s : node.getState().getNeighbours()) {
+            TreeNode neighbourTreeNode = new TreeNode(s, node);
+            min = Math.max(min, this.maximize(depth - 1, neighbourTreeNode, alpha, beta));
+            if (min < beta) beta = min;
+            if (alpha >= beta) break;
+        }
+
+        return min;
     }
 
     @Override
     public TreeNode getMinimaxTree() {
-        return null;
+        if (root == null) throw new NullPointerException("Can't get minimax tree before making the play");
+        return root;
     }
 
     @Override
     public int getNumberOfNodesExpanded() {
-        return 0;
+        return numberOfNodesExpanded;
     }
-
 }
--- src/main/java/com/quadcore/connectfour/model/ai/MinimaxWithoutPruning.java
+++ src/main/java/com/quadcore/connectfour/model/ai/MinimaxWithoutPruning.java
@@ -3,63 +3,62 @@ package com.quadcore.connectfour.model.ai;
 import com.quadcore.connectfour.model.datastructures.TreeNode;
 import com.quadcore.connectfour.model.state.State;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MinimaxWithoutPruning implements ConnectFourAI {
 
     private final int maxDepth;
-    private double maxScore = Double.MAX_VALUE;
     private TreeNode root = null;
+    private int numberOfNodesExpanded = 0;
 
 
     public MinimaxWithoutPruning(int maxDepth) {
+        if (maxDepth < 1) throw new IllegalArgumentException("Minimax depth can't be less than one");
         this.maxDepth = maxDepth;
     }
 
     @Override
     public TreeNode play(State state) {
-        int depth = maxDepth;
+        if (state.isTerminal()) throw new IllegalArgumentException("Can't play on a terminal state");
+        numberOfNodesExpanded = 1;
         root = new TreeNode(state, null);
         TreeNode optimalMove = null;
+
+        double maxScore = Double.MIN_VALUE;
+
         for (State s : state.getNeighbours()) {
             TreeNode stateTreeNode = new TreeNode(s, root);
-            double childVal = minimize(depth - 1, stateTreeNode);
+            double childVal = minimize(maxDepth - 1, stateTreeNode);
             if (childVal > maxScore) {
                 maxScore = childVal;
                 optimalMove = stateTreeNode;
             }
-
         }
 
         return optimalMove;
     }
 
     private double maximize(int depth, TreeNode node) {
-
+        numberOfNodesExpanded++;
         if (depth == 0 || node.getState().isTerminal())
             return node.getState().evaluate();
 
-        List<State> nextStates = node.getState().getNeighbours();
         double max = Double.MIN_VALUE;
-        for (State s : nextStates) {
+        for (State s : node.getState().getNeighbours()) {
             TreeNode neighbourTreeNode = new TreeNode(s, node);
             max = Math.max(max, this.minimize(depth - 1, neighbourTreeNode));
-
         }
 
         return max;
     }
 
     private double minimize(int depth, TreeNode node) {
+        numberOfNodesExpanded++;
         if (depth == 0 || node.getState().isTerminal())
             return node.getState().evaluate();
-        List<State> nextStates = node.getState().getNeighbours();
+
         double min = Double.MAX_VALUE;
-        for (State s : nextStates) {
+        for (State s : node.getState().getNeighbours()) {
             TreeNode neighbourTreeNode = new TreeNode(s, node);
             min = Math.max(min, this.maximize(depth - 1, neighbourTreeNode));
-
         }
 
         return min;
@@ -67,11 +66,12 @@ public class MinimaxWithoutPruning implements ConnectFourAI {
 
     @Override
     public TreeNode getMinimaxTree() {
-        return null;
+        if (root == null) throw new NullPointerException("Can't get minimax tree before making the play");
+        return root;
     }
 
     @Override
     public int getNumberOfNodesExpanded() {
-        return 0;
+        return numberOfNodesExpanded;
     }
 }
