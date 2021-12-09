cb74fd4bad5fd4abb3d97ab20cf4290d4b06c405
Ahmad Waleed <a.waleedothman@gmail.com>
2021-12-09 13:41:46 +0200

    Updated heuristics

--- src/main/java/com/quadcore/connectfour/model/heuristics/ConnectFourHeuristic.java
+++ src/main/java/com/quadcore/connectfour/model/heuristics/ConnectFourHeuristic.java
@@ -5,9 +5,12 @@ import com.quadcore.connectfour.model.state.State;
 
 
 public class ConnectFourHeuristic implements Heuristic {
-    private static final int LVL4 = 200;
+    private static final int LVL5 = 300;
+    private static final int LVL4 = 60;
     private static final int LVL3 = 40;
     private static final int LVL2 = 10;
+    private static final double DEFENSIVE_FACTOR = 0.2;
+    private static final double OVERLAPPING_FACTOR = 0.05;
 
 
     private char[][] state2D;
@@ -18,7 +21,7 @@ public class ConnectFourHeuristic implements Heuristic {
         state2D = state.to2dArray();
         double heuristic = 0;
         State.Score score = state.getScore();
-        heuristic += (score.getAIScore() - score.getPlayerScore()) * LVL4;
+        heuristic += (score.getAIScore() - score.getPlayerScore()) * LVL5;
         for (int i = 0; i < Game.ROWS; i++) {
             for (int j = 0; j < Game.COLUMNS; j++) {
                 if (state2D[i][j] == State.EMPTY && isValidMove(i, j)) {
@@ -31,29 +34,53 @@ public class ConnectFourHeuristic implements Heuristic {
     }
 
     private double getEmptySlotEvaluation(int row, int col, char player) {
-        //todo
-        return getHorizontalEmptyEvaluation(row, col, player)
-                + getVerticalEmptyEvaluation(row, col, player)
-                + getNegativeDiagonalEvaluation(row, col, player)
-                + getPositiveDiagonalEvaluation(row, col, player);
+        double horizontal = getHorizontalEmptyEvaluation(row, col, player);
+        double vertical = getVerticalEmptyEvaluation(row, col, player);
+        double positiveDiagonal = getPositiveDiagonalEvaluation(row, col, player);
+        double negativeDiagonal = getNegativeDiagonalEvaluation(row, col, player);
+        return getOverlappingFactor(horizontal, vertical, positiveDiagonal, negativeDiagonal) * (
+                horizontal + vertical + positiveDiagonal + negativeDiagonal
+                );
     }
 
-    private double getHeuristicScore(int run, int emptyLeft, int emptyRight, char player) {
+    private double getHeuristicScore(int run, int emptyLeft, int emptyRight,
+                                     boolean canPlayLeft, boolean canPlayRight, char player) {
         /*no way we can get 4*/
         if (run + emptyLeft + emptyRight < 4) return 0;
-        /*empty left, three filled, empty right*/
+
+        /*three filled, both sides empty*/
         if (run == 3 && emptyLeft > 0 && emptyRight > 0) {
-            if (player == State.PLAYER) return LVL4 * 1.5;
+            if (canPlayLeft && canPlayRight && player == State.PLAYER) return LVL5 * (1 + DEFENSIVE_FACTOR);
+            if (canPlayLeft && canPlayRight) return LVL5;
+            if ((canPlayLeft || canPlayRight) && player == State.PLAYER) return LVL4 * (1 + DEFENSIVE_FACTOR);
+            if (canPlayLeft || canPlayRight) return LVL4;
+            if (player == State.PLAYER) return LVL4 * (1 + DEFENSIVE_FACTOR);
             return LVL4;
         }
+
         /*three filled, empty from one side*/
-        if (run == 3) return LVL3;
-        if (run == 2 && emptyLeft > 0 && emptyRight > 0) return LVL3;
-        if (run == 2) return LVL2;
-        if (run >= 4) {
-            if (player == State.PLAYER) return LVL4 * 1.5;
-            return LVL4;
+        if (run == 3) {
+            if (canPlayLeft || canPlayRight) return LVL2;
+            return LVL3;
         }
+
+        /*two filled, empty both sides*/
+        if (run == 2 && emptyLeft > 0 && emptyRight > 0) {
+            if (canPlayLeft && canPlayRight) return LVL3;
+            if (canPlayLeft || canPlayRight) return LVL2;
+            return (3 * LVL2 + LVL3) / 4.0;
+        }
+        /*two filled, empty one sides*/
+        if (run == 2) {
+            if (canPlayLeft || canPlayRight) return LVL2;
+            return (3 * LVL2 + LVL3) / 4.0;
+        }
+
+        if (run >= 4) {
+            if (player == State.PLAYER) return LVL5 * (1 + DEFENSIVE_FACTOR);
+            return LVL5;
+        }
+
         return 0;
     }
 
@@ -66,19 +93,23 @@ public class ConnectFourHeuristic implements Heuristic {
             rowIndex++;
         }
 
-        return getHeuristicScore(count, row, 0, player);
+        return getHeuristicScore(count, row, 0, true, false, player);
     }
 
     private double getHorizontalEmptyEvaluation(int row, int col, char player) {
         int count = 1;
         int emptyLeft = 0;
         int emptyRight = 0;
+        boolean canPlayLeft = false;
+        boolean canPlayRight = false;
 
         int colIndex = col - 1;
         while (colIndex >= 0 && state2D[row][colIndex] == player) {
             count++;
             colIndex--;
         }
+        if (colIndex >= 0 && state2D[row][colIndex] == State.EMPTY
+                && isValidMove(row, colIndex)) canPlayLeft = true;
         while (colIndex >= 0 && state2D[row][colIndex] == State.EMPTY) {
             emptyLeft++;
             colIndex--;
@@ -89,18 +120,22 @@ public class ConnectFourHeuristic implements Heuristic {
             count++;
             colIndex++;
         }
+        if (colIndex < Game.COLUMNS && state2D[row][colIndex] == State.EMPTY
+                && isValidMove(row, colIndex)) canPlayRight = true;
         while (colIndex < Game.COLUMNS && state2D[row][colIndex] == State.EMPTY) {
             emptyRight++;
             colIndex++;
         }
 
-        return getHeuristicScore(count, emptyLeft, emptyRight, player);
+        return getHeuristicScore(count, emptyLeft, emptyRight, canPlayLeft, canPlayRight, player);
     }
 
     private double getPositiveDiagonalEvaluation(int row, int col, char player) {
         int count = 1;
         int emptyLeft = 0;
         int emptyRight = 0;
+        boolean canPlayLeft = false;
+        boolean canPlayRight = false;
 
         int colIndex = col + 1;
         int rowIndex = row - 1;
@@ -109,6 +144,8 @@ public class ConnectFourHeuristic implements Heuristic {
             colIndex++;
             rowIndex--;
         }
+        if (colIndex < Game.COLUMNS && rowIndex >= 0 && state2D[rowIndex][colIndex] == State.EMPTY
+                && isValidMove(rowIndex, colIndex)) canPlayRight = true;
         while (colIndex < Game.COLUMNS && rowIndex >= 0 && state2D[rowIndex][colIndex] == State.EMPTY) {
             emptyRight++;
             colIndex++;
@@ -122,19 +159,23 @@ public class ConnectFourHeuristic implements Heuristic {
             colIndex--;
             rowIndex++;
         }
+        if (colIndex >= 0 && rowIndex < Game.ROWS && state2D[rowIndex][colIndex] == State.EMPTY
+                && isValidMove(rowIndex, colIndex)) canPlayLeft = true;
         while (colIndex >= 0 && rowIndex < Game.ROWS && state2D[rowIndex][colIndex] == State.EMPTY) {
             emptyLeft++;
             colIndex--;
             rowIndex++;
         }
 
-        return getHeuristicScore(count, emptyLeft, emptyRight, player);
+        return getHeuristicScore(count, emptyLeft, emptyRight, canPlayLeft, canPlayRight, player);
     }
 
     private double getNegativeDiagonalEvaluation(int row, int col, char player) {
         int count = 1;
         int emptyLeft = 0;
         int emptyRight = 0;
+        boolean canPlayLeft = false;
+        boolean canPlayRight = false;
 
         int colIndex = col - 1;
         int rowIndex = row - 1;
@@ -143,6 +184,8 @@ public class ConnectFourHeuristic implements Heuristic {
             colIndex--;
             rowIndex--;
         }
+        if (colIndex >= 0 && rowIndex >= 0 && state2D[rowIndex][colIndex] == State.EMPTY
+                && isValidMove(rowIndex, colIndex)) canPlayLeft = true;
         while (colIndex >= 0 && rowIndex >= 0 && state2D[rowIndex][colIndex] == State.EMPTY) {
             emptyLeft++;
             colIndex--;
@@ -156,17 +199,28 @@ public class ConnectFourHeuristic implements Heuristic {
             colIndex++;
             rowIndex++;
         }
+        if (colIndex < Game.COLUMNS && rowIndex < Game.ROWS && state2D[rowIndex][colIndex] == State.EMPTY
+                && isValidMove(rowIndex, colIndex)) canPlayRight = true;
         while (colIndex < Game.COLUMNS && rowIndex < Game.ROWS && state2D[rowIndex][colIndex] == State.EMPTY) {
             emptyRight++;
             colIndex++;
             rowIndex++;
         }
 
-        return getHeuristicScore(count, emptyLeft, emptyRight, player);
+        return getHeuristicScore(count, emptyLeft, emptyRight, canPlayLeft, canPlayRight, player);
     }
 
     private boolean isValidMove(int row, int col) {
         if (row == Game.ROWS - 1) return true;
         return state2D[row + 1][col] != State.EMPTY;
+    }
+
+    private double getOverlappingFactor(double a, double b, double c, double d) {
+        int count = 0;
+        if (a > 0) count++;
+        if (b > 0) count++;
+        if (c > 0) count++;
+        if (d > 0) count++;
+        return 1 + count * OVERLAPPING_FACTOR;
     }
 }
